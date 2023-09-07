6e2a6c3ea95746eb29535b950226bfbfce463d05
SoheilRezaei <soheilrezaei966+github@gmail.com>
2023-09-07 11:24:39 -0400

    Handling http request and respone

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,13 @@
 import express from "express";
 
-console.log(express);
+const server = express();
+
+server.use(express.static("dist"));
+
+server.use("/", (req, res) => {
+  res.send("Hello Express!");
+});
+
+server.listen("8080", "localhost", () => {
+  console.info("Express server is listening at http://localhost:8080");
+});
