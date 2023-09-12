1c61370776045e7fccae899faf650d8c8349d66d
SoheilRezaei <soheilrezaei966+github@gmail.com>
2023-09-12 09:13:40 -0400

    useState and useEffect

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+
 import Header from "./header";
 
 const App = () => {
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setCounter(counter + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  });
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="container">
       <Header message="Naming Contests" />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        {counter}
+      </button>
     </div>
   );
 };
