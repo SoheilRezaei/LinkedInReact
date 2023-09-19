67e20044718c61ec3a0c3f76c9829867934ba333
SoheilRezaei <soheilrezaei966+github@gmail.com>
2023-09-19 08:55:10 -0400

    Mock data no longer needed

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,14 +1,35 @@
 import express from "express";
 import cors from "cors";
-import testData from "../test-data.json";
+import { connectClient } from "./db";
 
 const router = express.Router();
 
 router.use(cors());
 
-router.get("/contests", (req, res) => {
-  //get data from mongoDB
-  res.send({ contests: testData });
+router.get("/contests", async (req, res) => {
+const client = await connectClient();
+
+const contests = await client.collection('contests')
+  .find()
+  .project({
+    id: 1,
+    categoryName: 1,
+    contestName: 1,
+    _id:0
+})
+  .toArray();
+
+  res.send({ contests });
 });
+
+router.get("/contest/:contestId", async (req, res) => {
+  const client = await connectClient();
+
+  const contest = await client
+    .collection("contests")
+    .findOne({id: req.params.contestId})
+
+  res.send({contest})
+})
 
 export default router;
